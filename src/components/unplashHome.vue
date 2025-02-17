a6a6f520e0e8d490c618a1bea6c52423f8467929
Timi Precious <pt.timone@gmail.com>
2025-02-17 19:03:11 +0100

    initialize Vue 3 + TypeScript + Vite project with basic structure and configuration

---
+++ src/components/unplashHome.vue
@@ -0,0 +1,213 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watchEffect } from "vue";
+import axios from "axios";
+import { type Photo } from "../types/index";
+
+const props = defineProps<{ msg: string }>();
+
+const photos = ref<Photo[]>([]);
+const inputQuery = ref<string>("");
+const query = ref<string>("");
+const isSearchResults = ref(false);
+const fetchInitialPhotos = async () => {
+    const response = await axios.get<Photo[]>(
+        `https://api.unsplash.com/photos/?&client_id=${
+            import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+        }`
+    );
+
+    photos.value = response.data;
+};
+const fetchPhotos = async () => {
+    const response = await axios.get<{ results: Photo[] }>(
+        `https://api.unsplash.com/search/photos/?page=1&query=${
+            query.value
+        }&client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`
+    );
+    isSearchResults.value = true;
+    photos.value = response.data.results;
+};
+
+function searchPhotos() {
+    console.log(inputQuery.value);
+    query.value = inputQuery.value;
+    fetchPhotos();
+}
+
+onMounted(() => {
+    fetchInitialPhotos();
+});
+
+// watchEffect(() => {
+//     fetchPhotos();
+// });
+</script>
+
+<template>
+    <div class="page">
+        <div class="header-container">
+            <div class="header-content">
+                <div class="search-content" v-if="isSearchResults">
+                    Search Results for
+                    <span class="query">"{{ inputQuery }}"</span>
+                </div>
+                <div v-else class="input-container">
+                    <div class="input-box">
+                        <div class="search-icon">
+                            <img src="../assets/search-normal.svg" alt="">
+
+                        </div>
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            v-model="inputQuery"
+                        />
+                    </div>
+                    <button @click="searchPhotos">Search</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="img-container">
+            <div v-for="item in photos" :key="item.id" class="image-box">
+                <!-- {{ item }} -->
+                <div class="">
+                    <img :src="item.urls.small_s3" alt="" />
+                </div>
+                <div class="img-meta">
+                    <h3>{{ item.user.name }}</h3>
+                    <p>{{ item.alt_description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.page {
+    position: relative;
+    top: 0;
+    width: 100vw;
+    color: black;
+    & .header-container {
+        background-color: #eaf0f2;
+        width: 100%;
+        height: max-content;
+
+        padding: 120px 0;
+        & .header-content {
+            width: 80%;
+            margin: 0 auto;
+            & .search-content {
+                font-weight: 600;
+                color: #455d65;
+                font-size: 48px;
+                & .query {
+                    color: #9ac0cd;
+                }
+            }
+        }
+        & .input-container {
+            display: flex;
+            flex-direction: column;
+            gap: 12px;
+            & .input-box {
+                background-color: white;
+
+                border: none;
+                height: min-content;
+                border-radius: 10px;
+                overflow: hidden;
+                position: relative;
+                width: 100%;
+                height: 54px;
+                & .search-icon {
+                    position: relative;
+                    top: 50%;
+                    translate: 0 -50%;
+                    height: max-content;
+                    padding-left: 5px;
+                    width: max-content;
+                }
+
+                & input {
+                    border: none;
+                    color: #1a1a1a;
+                    // padding: 18px 12px;
+                    height: 100%;
+                    padding-left: 60px;
+                    outline: none;
+                    background-color: transparent;
+                    position: absolute;
+                    z-index: 20;
+                    top: 0;
+                    translate: 0 0%;
+                    width: 100%;
+                }
+            }
+            & button {
+                background-color: #1a1a1a;
+                max-width: max-content;
+                color: white;
+                border: none;
+                border-radius: 10px;
+                padding: 12px 24px;
+                font-size: 1rem;
+                cursor: pointer;
+                transition: 0.3s;
+                &:hover {
+                    background-color: #2a2a2a;
+                }
+            }
+        }
+    }
+    & .img-container {
+        display: grid;
+        grid-template-columns: repeat(3, 300px);
+        padding-inline: 24px;
+        translate: 0 -30px;
+        row-gap: 28px;
+        column-gap: 64px;
+        width: max-content;
+        max-width: 70vw;
+        margin: auto;
+
+        & .image-box {
+            width: 100%;
+            height: max-content;
+            // border: solid;
+            overflow: hidden;
+            border-radius: 10px;
+            position: relative;
+
+            & img {
+                position: relative;
+                z-index: 10;
+                width: 100%;
+                border-radius: 10px;
+
+                height: 100%;
+                object-fit: cover;
+            }
+            & .img-meta {
+                position: absolute;
+                z-index: 50;
+                color: white;
+                bottom: 10%;
+                padding-inline: 6px;
+                text-shadow: 0.1px 0.1px 2px #1a1a1a;
+
+                & h3 {
+                    font-size: 1.25rem;
+                    font-weight: 600;
+                    margin: 0;
+                }
+                & p {
+                    margin: 0;
+                    font-size: 0.85rem;
+                }
+            }
+        }
+    }
+}
+</style>
